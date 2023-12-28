595868ac6bb5a3600091df4dd583e52884497202
LGTM-Braxton <93713052+B4D-TP@users.noreply.github.com>
2023-12-28 16:26:05 -0500

    Fix: Everything

--- Tests/Public/Test-Function1.Tests.ps1
+++ Tests/Public/Test-Function1.Tests.ps1
@@ -1,6 +1,12 @@
 BeforeAll {
-    #Module is Downloaded from Github Artifacts and in the module folder
-    Import-Module -Name $PSScriptRoot/../Modules/demomodule
+    $ProjectRoot = Split-Path $(Split-Path $PSScriptRoot -Parent) -Parent
+    $ModuleName = $(Get-ChildItem -Path (Join-Path $ProjectRoot "Source") -Filter "*.psm1").Name.Replace(".psm1", "")
+    $ModulePath = Join-Path $(Join-Path $ProjectRoot "Source") "$ModuleName.psm1"
+
+    Get-Module -Name $ModuleName -All | Remove-Module -Force -ErrorAction Ignore
+    Import-Module -Name $ModulePath -Force -ErrorAction Stop
+
+    $NL = [System.Environment]::NewLine
 }
 
 Describe "Test-Function1" {
